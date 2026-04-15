b5816a702be189ed86184cc9d5c605bdff5329e2
corredor29 <197847818+corredor29@users.noreply.github.com>
2026-04-14 20:00:17 -0500

    feat: :sparkles: se agrego dbcontext para coneccion a base de datos

--- obj/Debug/net10.0/GestorDeVuelosProyectoFinal.AssemblyInfo.cs
+++ obj/Debug/net10.0/GestorDeVuelosProyectoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorDeVuelosProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841ecd4fecff338d1d687964cf20edfe36e65ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ea869e0207667e99a6657e5cf7dad0430419f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorDeVuelosProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorDeVuelosProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
